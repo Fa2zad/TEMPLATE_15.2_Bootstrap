48d4418f34063ad5ccd78e196a9ae0b152ed859b
محسن نیکنام <niknam@faratech.com>
2018-07-16 13:02:30 +0430

    Content

---
+++ assets/js/scripts.js
@@ -0,0 +1,9 @@
+// $(document).ready(function () {
+//     $(".contents__container ul li").hover(function(){
+//         alert(event);
+//         event.css("background-color", "yellow");
+
+//     });
+
+    
+// });
